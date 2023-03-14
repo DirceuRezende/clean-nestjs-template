bf365cd6bcb7f938973e84ed842a4bcfa6162842
Dirceu Rezende <dirceu.srezende@gmail.com>
2023-03-13 21:36:51 -0300

    test: create UserInMemoryRepository

---
+++ src/@core/infra/db/in-memory/user-in-memory.repository.spec.ts
@@ -0,0 +1,98 @@
+import { User } from '@/@core/domain/entities';
+import { UserInMemoryRepository } from './user-in-memory.repository';
+
+describe('UserInMemoryRepository', () => {
+  describe('addUser', () => {
+    it('should add a new user', () => {
+      const user = User.create({
+        email: 'any@example.com',
+        emailVerified: false,
+        name: 'any',
+        password: 'any_password',
+      }).value as User;
+
+      const repository = new UserInMemoryRepository([]);
+
+      repository.addUser(user);
+
+      expect(repository.items[0]).toEqual(user);
+    });
+  });
+
+  describe('getUserByEmail', () => {
+    it('should obtain user by e-mail', async () => {
+      const user = User.create({
+        email: 'any@example.com',
+        emailVerified: false,
+        name: 'any',
+        password: 'any_password',
+      }).value as User;
+
+      const repository = new UserInMemoryRepository([user]);
+
+      const userFound = await repository.getUserByEmail('any@example.com');
+
+      expect(userFound).toEqual(user);
+    });
+  });
+
+  describe('getUserByUserId', () => {
+    it('should obtain user by id', async () => {
+      const user = User.create({
+        id: 1,
+        email: 'any@example.com',
+        emailVerified: false,
+        name: 'any',
+        password: 'any_password',
+      }).value as User;
+
+      const repository = new UserInMemoryRepository([user]);
+
+      const userFound = await repository.getUserByUserId(user.id);
+
+      expect(userFound).toEqual(user);
+    });
+  });
+
+  describe('updateUser', () => {
+    it('should update existing user', async () => {
+      const user = User.create({
+        id: 1,
+        email: 'any@example.com',
+        emailVerified: false,
+        name: 'any',
+        password: 'any_password',
+      }).value as User;
+
+      const repository = new UserInMemoryRepository([user]);
+
+      user.verifyEmail();
+      await repository.updateUser(user);
+
+      expect(repository.items[0].emailVerified).toBeTruthy();
+    });
+
+    it('should not update non existing user', async () => {
+      const user1 = User.create({
+        id: 1,
+        email: 'any@example.com',
+        emailVerified: false,
+        name: 'any',
+        password: 'any_password',
+      }).value as User;
+
+      const repository = new UserInMemoryRepository([user1]);
+
+      const user2 = User.create({
+        id: 2,
+        email: 'any@example.com',
+        emailVerified: false,
+        name: 'any',
+        password: 'any_password',
+      }).value as User;
+      await repository.updateUser(user2);
+
+      expect(repository.items[0].emailVerified).toBeFalsy();
+    });
+  });
+});

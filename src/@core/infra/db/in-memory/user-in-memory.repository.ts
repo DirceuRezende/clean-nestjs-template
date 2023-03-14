2129fc4c2fecf75a843f56c93499b6e6aaf68177
Dirceu Rezende <dirceu.srezende@gmail.com>
2023-03-13 21:36:05 -0300

    feat: create UserInMemoryRepository

---
+++ src/@core/infra/db/in-memory/user-in-memory.repository.ts
@@ -0,0 +1,33 @@
+import { UserRepository } from '@/@core/application/ports/repositories';
+import { User } from '@/@core/domain/entities';
+
+export class UserInMemoryRepository implements UserRepository {
+  constructor(public items: User[] = []) {}
+
+  getUserByEmail(email: string): Promise<User> {
+    return new Promise((resolve) => {
+      const user = this.items.filter((user) => user.email === email)[0];
+
+      resolve(user);
+    });
+  }
+  updateUser(user: User): Promise<void> {
+    const index = this.items.findIndex((item) => item.id === user.id)[0];
+    if (index) {
+      this.items[index] = user;
+    }
+
+    return;
+  }
+  getUserByUserId(userId: number): Promise<User> {
+    return new Promise((resolve) => {
+      const user = this.items.filter((user) => user.id === userId)[0];
+
+      resolve(user);
+    });
+  }
+  addUser(user: User): Promise<void> {
+    this.items.push(user);
+    return;
+  }
+}

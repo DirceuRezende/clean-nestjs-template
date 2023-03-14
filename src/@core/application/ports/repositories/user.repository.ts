ff36d833bd1280b6917bd711f75541e909cfcc77
Dirceu Rezende <dirceu.srezende@gmail.com>
2023-03-13 21:31:35 -0300

    refactor: change method name createUser to addUser

--- src/@core/application/ports/repositories/user.repository.ts
+++ src/@core/application/ports/repositories/user.repository.ts
@@ -4,5 +4,5 @@ export interface UserRepository {
   getUserByEmail(email: string): Promise<User>;
   updateUser(user: User): Promise<void>;
   getUserByUserId(userId: number): Promise<User>;
-  createUser(user: User): Promise<User>;
+  addUser(user: User): Promise<void>;
 }
